dxdxdt/exfatprogs

commit 5ab91a8d949b29f294a26372c7484cf2fa4ef6e9

Do not run on pre-2.6 Linux kernel
The sysfs interfaces are crucial for mkfs to discover the
characteristics of the target device being formatted. To safely rely on
sysfs interface, impose the minimum kernel version supported to avoid
potential undefined behaviour.

Signed-off-by: David Timber <dxdt@dev.snart.me>
Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

lib/libexfat.c
@@ -15,6 +15,7 @@
 #ifdef _POSIX_MAPPED_FILES
 #include <sys/mman.h>
 #endif
+#include <sys/utsname.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -146,13 +147,79 @@ void init_user_input(struct exfat_user_input *ui)
 	ui->discard = true;
 }
 
+static size_t count_dots(const char *s)
+{
+	size_t ret = 0;
+
+	while (*s) {
+		if (*s == '.')
+			ret++;
+		s++;
+	}
+
+	return ret;
+}
+
+static int exfat_cmp_kernel_ver(const unsigned short *req)
+{
+	struct utsname uts;
+	unsigned short v[3] = { 0, };
+	unsigned long long nreq = 0, nhost = 0;
+	int ret;
+
+	ret = uname(&uts);
+	if (ret)
+		return -1;
+
+	switch (count_dots(uts.release)) {
+	case 0:
+		ret = sscanf(uts.release, "%hu", v) != 1;
+		break;
+	case 1:
+		ret = sscanf(uts.release, "%hu.%hu", v + 0, v + 1) != 2;
+		break;
+	default:
+		ret = sscanf(uts.release, "%hu.%hu.%hu", v + 0, v + 1, v + 2) != 3;
+	}
+	if (ret) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	exfat_debug("Kernel version: %hu.%hu.%hu\n", v[0], v[1], v[2]);
+
+	nreq  |= (unsigned long long)req[0] << 32;
+	nreq  |= (unsigned long long)req[1] << 16;
+	nreq  |= (unsigned long long)req[2];
+	nhost |= (unsigned long long)v[0]   << 32;
+	nhost |= (unsigned long long)v[1]   << 16;
+	nhost |= (unsigned long long)v[2];
+
+	return nreq > nhost ? 1 : 0;
+}
+
 int exfat_get_blk_dev_info(struct exfat_user_input *ui,
 		struct exfat_blk_dev *bd)
 {
+	static const unsigned short MIN_KERNEL_VER[3] = { 2, 6, 0 };
 	int fd, ret = -1;
 	off_t blk_dev_size;
 	struct stat st;
 	unsigned long long blk_dev_offset = 0;
+
+	/* Assert kernel version >= 2.6 since a lot of interfaces in /sys/dev/block/ is relied upon.
+	 * Other fs utils written in pre-2.6 had to make a lot of "juggling" to figure out the
+	 * characteristics of the target block device. As exFAT is developed in 2010's, we can enjoy
+	 * the luxury of the relatively recent sysfs interfaces at our disposal.
+	 */
+	switch (exfat_cmp_kernel_ver(MIN_KERNEL_VER)) {
+	case -1:
+		exfat_err("failed to get kernel version.\n");
+		return -1;
+	case 1:
+		exfat_err("pre-2.6 kernel version not supported\n");
+		return -1;
+	}
 
 	fd = open(ui->dev_name, ui->writeable ? O_RDWR|O_EXCL : O_RDONLY);
 	if (fd < 0) {

commit 10736d7ff0f7517bb7e570cbb323e56669ee238f

libexfat: remove use of sscanf()
Due to a bug inherent in the C programming language itself, the *scanf()
functions are unsafe because they exhibit undefined behaviour in error
cases. The recent patches introduced functions that use sscanf(),
leading to some input sanitisation issues.

Remove the use of sscanf() by replacing it with strto*() functions. Add
additional exfat_parse_*() functions using macros. Add tests to refactor
exfat_cmp_kernel_ver().

The environment variables that take boolean values are now properly
validated before use.

Link: https://github.com/exfatprogs/exfatprogs/pull/380#discussion_r3671267926
Fixes: 5ab91a8d949b ("Do not run on pre-2.6 Linux kernel")
Signed-off-by: David Timber <dxdt@dev.snart.me>

include/libexfat.h
@@ -317,7 +317,17 @@ int exfat_o2c(struct exfat *exfat, off_t device_offset,
 bool exfat_heap_clus(struct exfat *exfat, clus_t clus);
 int exfat_root_clus_count(struct exfat *exfat);
 int read_boot_sect(struct exfat_blk_dev *bdev, struct pbr **bs);
-int exfat_parse_ulong(const char *s, unsigned long *out);
+
+#define DECLARE_PARSE_NUM(OUT_T, NAME) int NAME(const char *s, OUT_T *out)
+DECLARE_PARSE_NUM(unsigned long, exfat_parse_ulong);
+DECLARE_PARSE_NUM(unsigned long long, exfat_parse_ulonglong);
+DECLARE_PARSE_NUM(long, exfat_parse_long);
+DECLARE_PARSE_NUM(long long, exfat_parse_longlong);
+#undef DECLARE_PARSE_NUM
+
+int exfat_parse_swver(const char *in, unsigned short *out);
+int exfat_cmp_swver(const unsigned short *a, const unsigned short *b);
+
 int exfat_check_name(__le16 *utf16_name, int len);
 /*
  * Read back from the target device to confirm the successful write.

lib/Makefile.am
@@ -5,9 +5,12 @@ libexfat_a_SOURCES = libexfat.c exfat_fs.c exfat_dir.c utils.c
 
 if ENABLE_TESTS
 
-noinst_PROGRAMS = tests/suite0000-bitmap
+noinst_PROGRAMS = tests/suite0000-bitmap tests/suite0001-parse
 
 tests_suite0000_bitmap_SOURCES = tests/suite0000-bitmap.c
 tests_suite0000_bitmap_LDADD = libexfat.a
+
+tests_suite0001_parse_SOURCES = tests/suite0001-parse.c
+tests_suite0001_parse_LDADD = libexfat.a
 
 endif

lib/libexfat.c
@@ -178,9 +178,9 @@ static int get_intbool_envvar(const char *name)
 	const char *env = getenv(name);
 
 	if (env != NULL) {
-		int v = -1;
+		long v = -1;
 
-		if (sscanf(env, "%d", &v) == 1)
+		if (exfat_parse_long(env, &v) == 0)
 			return v != 0;
 	}
 
@@ -477,39 +477,16 @@ static bool exfat_dir_has_child(const int at, const char *path)
 static int exfat_cmp_kernel_ver(const unsigned short *req)
 {
 	struct utsname uts;
-	unsigned short v[3] = { 0, };
-	unsigned long long nreq = 0, nhost = 0;
+	unsigned short parsed[3];
 	int ret;
 
-	ret = uname(&uts);
-	if (ret)
+	if (uname(&uts) || exfat_parse_swver(uts.release, parsed))
 		return -1;
 
-	switch (count_dots(uts.release, 65)) {
-	case 0:
-		ret = sscanf(uts.release, "%hu", v) != 1;
-		break;
-	case 1:
-		ret = sscanf(uts.release, "%hu.%hu", v + 0, v + 1) != 2;
-		break;
-	default:
-		ret = sscanf(uts.release, "%hu.%hu.%hu", v + 0, v + 1, v + 2) != 3;
-	}
-	if (ret) {
-		errno = EINVAL;
-		return -1;
-	}
+	exfat_debug("Kernel version: %u.%u.%u\n", parsed[0], parsed[1], parsed[2]);
 
-	exfat_debug("Kernel version: %hu.%hu.%hu\n", v[0], v[1], v[2]);
-
-	nreq  |= (unsigned long long)req[0] << 32;
-	nreq  |= (unsigned long long)req[1] << 16;
-	nreq  |= (unsigned long long)req[2];
-	nhost |= (unsigned long long)v[0]   << 32;
-	nhost |= (unsigned long long)v[1]   << 16;
-	nhost |= (unsigned long long)v[2];
-
-	return nreq > nhost ? 1 : 0;
+	ret = exfat_cmp_swver(req, parsed);
+	return ret > 0 ? 1 : 0;
 }
 
 int exfat_get_blk_dev_info(struct exfat_user_input *ui,
@@ -1936,20 +1913,80 @@ err:
 	return err;
 }
 
-int exfat_parse_ulong(const char *s, unsigned long *out)
+/* Abstraction for exfat_parse_*() */
+#define DEFINE_PARSE_NUM(OUT_T, NAME, STRTO_F)		\
+	int NAME(const char *s, OUT_T *out)		\
+	{						\
+		const int saved_errno = errno;		\
+		int ret = 0;				\
+		char *endptr = NULL;			\
+							\
+		errno = 0;				\
+		*out = STRTO_F(s, &endptr, 0);		\
+		if (errno)				\
+			ret = -errno;			\
+		if (s == endptr || *endptr != '\0')	\
+			ret = -EINVAL;			\
+							\
+		errno = saved_errno;			\
+		return ret;				\
+	}
+
+DEFINE_PARSE_NUM(unsigned long, exfat_parse_ulong, strtoul)
+DEFINE_PARSE_NUM(unsigned long long, exfat_parse_ulonglong, strtoull)
+DEFINE_PARSE_NUM(long, exfat_parse_long, strtol)
+DEFINE_PARSE_NUM(long long, exfat_parse_longlong, strtoll)
+
+#undef DEFINE_PARSE_NUM
+
+int exfat_parse_swver(const char *in, unsigned short *out)
 {
-	char *endptr;
+	const int saved_errno = errno;
+	char *endptr = NULL;
+	unsigned long v;
+	unsigned short ret[3] = {0};
 
-	errno = 0;
+	if (*in == 0)
+		goto inval;
 
-	*out = strtoul(s, &endptr, 0);
+	for (size_t i = 0; i < 3 && *in != 0; i++) {
+		errno = 0;
+		v = strtoul(in, &endptr, 10);
+		if (errno != 0 || in == endptr || v > UINT16_MAX)
+			goto inval;
+		ret[i] = (unsigned short)v;
 
-	if (errno)
-		return -errno;
+		if (*endptr == '.')
+			in = endptr + 1;
+		else
+			break;
+	}
 
-	if (s == endptr || *endptr != '\0')
-		return -EINVAL;
+	out[0] = ret[0];
+	out[1] = ret[1];
+	out[2] = ret[2];
+	errno = saved_errno;
+	return 0;
+inval:
+	errno = EINVAL;
+	return -1;
+}
 
+int exfat_cmp_swver(const unsigned short *in_a, const unsigned short *in_b)
+{
+	uint64_t a = 0, b = 0;
+
+	a |= (uint64_t)in_a[0] << 32;
+	a |= (uint64_t)in_a[1] << 16;
+	a |= (uint64_t)in_a[2];
+	b |= (uint64_t)in_b[0] << 32;
+	b |= (uint64_t)in_b[1] << 16;
+	b |= (uint64_t)in_b[2];
+
+	if (a < b)
+		return -1;
+	else if (a > b)
+		return 1;
 	return 0;
 }
 

lib/tests/suite0001-parse.c
@@ -1,0 +1,82 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include "exfat_ondisk.h"
+#include "libexfat.h"
+
+#include <assert.h>
+
+#define SET_SWVER(o, a, b, c)		\
+	do {				\
+		(o)[0] = (a);		\
+		(o)[1] = (b);		\
+		(o)[2] = (c);		\
+	} while (0)
+
+static void test_swver_cmp(void)
+{
+	unsigned short first[3], second[3];
+#define CMP_VER(a, b, c, CMP, d, e, f)\
+	do {								\
+		SET_SWVER(first, (a), (b), (c));			\
+		SET_SWVER(second, (d), (e), (f));			\
+		assert(exfat_cmp_swver(first, second) CMP 0);		\
+	} while (0)
+
+	CMP_VER(0, 0, 0, ==, 0, 0, 0);
+	CMP_VER(2, 6, 0, ==, 2, 6, 0);
+	CMP_VER(1, 2, 3, ==, 1, 2, 3);
+
+	CMP_VER(2, 6, 0, >, 2, 0, 0);
+	CMP_VER(2, 6, 0, >, 2, 4, 0);
+	CMP_VER(2, 6, 0, >, 2, 4, 999);
+
+	CMP_VER(0, 0, 1, <, 65535, 65535, 65535);
+	CMP_VER(2, 6, 0, <, 2, 6, 1);
+	CMP_VER(2, 6, 0, <, 3, 0, 0);
+	CMP_VER(2, 6, 0, <, 7, 0, 0);
+	CMP_VER(2, 6, 0, <, 7, 2, 0);
+	CMP_VER(2, 6, 0, <, 7, 1, 8);
+	CMP_VER(2, 6, 0, <, 6, 18, 44);
+	CMP_VER(2, 6, 0, <, 6, 12, 103);
+	CMP_VER(2, 6, 0, <, 6, 6, 151);
+	CMP_VER(2, 6, 0, <, 6, 1, 182);
+	CMP_VER(2, 6, 0, <, 5, 15, 215);
+	CMP_VER(2, 6, 0, <, 5, 10, 0);
+}
+
+static void test_swver_parse(void)
+{
+	unsigned short ver[3];
+	int ret;
+#define EXPECT_VER(s, a, b, c)\
+	do {									\
+		ret = exfat_parse_swver((s), ver);				\
+		assert(ret == 0);						\
+		assert((a) == ver[0] && (b) == ver[1] && (c) == ver[2]);	\
+	} while (0)
+
+	assert(exfat_parse_swver("", ver));
+	assert(exfat_parse_swver("asd", ver));
+	assert(exfat_parse_swver("asd-1.2.3", ver));
+	assert(exfat_parse_swver("0.0.0", ver) == 0);
+	assert(exfat_parse_swver("7.1.8-200.fc44.x86_64", ver) == 0);
+	assert(exfat_parse_swver("2.6.0", ver) == 0);
+	assert(exfat_parse_swver("2.6.0aaaa", ver) == 0);
+
+	EXPECT_VER("6.1.157-android14-11-gbd23337e42e7-ab14791245", 6, 1, 157);
+	EXPECT_VER("5.4.242-32179049-abG990EXXSNHZF5", 5, 4, 242);
+	EXPECT_VER("6.6.77-8.el10.altarch.aarch64+64k", 6, 6, 77);
+	EXPECT_VER("7.1.8-200.fc44.x86_64", 7, 1, 8);
+	EXPECT_VER("6.17.0-1019-aws", 6, 17, 0);
+	EXPECT_VER("6.18.39-0-virt", 6, 18, 39);
+	EXPECT_VER("0", 0, 0, 0);
+	EXPECT_VER("42", 42, 0, 0);
+#undef EXPECT_VER
+}
+
+int main(void)
+{
+	test_swver_cmp();
+	test_swver_parse();
+
+	return 0;
+}